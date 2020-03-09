9ac357cdf612d5187431839fb6f85fe8cef71f5e
Adam Arezza <arezza.adam@gmail.com>
2020-03-08 22:09:10 -0400

    pibot-ui repo first commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
     <VideoStream></VideoStream>
     <Controls></Controls>
   </div>
@@ -17,12 +16,16 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #41ec4f;
+  background: black;
+  min-height: 100vh;
 }
 </style>
--- src/components/controls.vue
+++ src/components/controls.vue
@@ -1,27 +1,62 @@
 <template>
     <div id="controls">
+        <p>Connection: {{connected}}</p>
         <div class="key-grid">
-            <button v-for="(dir, index) in directions" :key="index" v-bind:class="dir" @click="robotMove(dir)" class="move-btn">{{dir.toUpperCase()}}</button>
-        </div>
-        <div class="key-grid">
-        </div>
-        <div class="key-grid">
+            <button v-for="(dir, index) in directions" :key="index" v-bind:class="[dir, {'highlight': dir == direction}]" class="move-btn">{{dir.toUpperCase()}}</button>
         </div>
     </div>
 </template>
 
 <script>
-import axios from 'axios'
 
 export default {
     data () {
         return {
-            directions: ['forward', 'reverse', 'left', 'right']
+            directions: ['forward', 'reverse', 'left', 'right'],
+            messages: undefined,
+            connected: false,
+            direction: undefined
+        }
+    },
+    sockets: {
+        connect() {
+            this.connected = true
         }
     },
     methods: {
+        getDirection(e) {
+            if(e.key == "w") {
+                this.direction = "forward"
+            }
+            if(e.key == "a") {
+                this.direction = "left"
+            }
+            if(e.key == "d") {
+                this.direction = "right"
+            }
+            if(e.key == "s") {
+                this.direction = "reverse"
+            }
+        },
         robotMove(direction) {
-            console.log(direction)
+            this.$socket.emit('move', this.direction)
+            console.log(this.direction)
+        },
+        stopMove() {
+            console.log("stopping")
+            this.direction = undefined
+            this.$socket.emit('move', "stop")
+        }
+    },
+    created() {
+        window.addEventListener('keypress', this.getDirection)
+        window.addEventListener('keyup', this.stopMove)
+    },
+    watch: {
+        direction: function(newDir, oldDir) {
+            if(newDir != oldDir) {
+                this.robotMove(newDir)
+            }
         }
     }
 }
@@ -37,7 +72,7 @@ export default {
     margin: 20px;
     display: grid;
     grid-template-columns: repeat(3, 100px);
-    grid-template-rows: repeat(3, 100px);
+    grid-template-rows: repeat(3, 50px);
 }
 .forward {
     grid-column-start: 2; 
@@ -56,5 +91,17 @@ export default {
 }
 .move-btn {
     font-size: 1em;
+    background: grey;
+    border: none;
+    color: greenyellow;
 }
+.move-btn:hover {
+    cursor: pointer;
+    border: solid 2px white;
+}
+
+.highlight {
+    background: white;
+}
+
 </style>
